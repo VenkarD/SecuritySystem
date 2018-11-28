9d51009f9dc65af0752fd368cfb0c11430c1b2f9
Ruslan Komarichev <meowhellyeah@gmail.com>
2018-11-28 13:47:44 +0300

    Borders drawing mode improved

--- frame_analysis/border_detector.py
+++ frame_analysis/border_detector.py
@@ -5,27 +5,38 @@ from datetime import datetime
 
 
 # сделать staticmethod/classmethod?
-def mouse_drawing(event, x, y, flags, circles):
+def mouse_drawing(event, x, y, flags, detector):
     if event == cv2.EVENT_LBUTTONDOWN:
         print("Left click")
-        circles.append((x, y))
-        print(len(circles), 'circles')
+        detector.circles.append((x, y))
+        print(len(detector.circles), 'circles')
+    elif event == cv2.EVENT_MOUSEMOVE:
+        detector.next_circle = (x, y)
 
 class BorderDetector:
     def __init__(self):
-        # сделать circles изначально np.ndarraym чтобы каждый раз не превращать в points?
+        # сделать circles изначально np.ndarray чтобы каждый раз не превращать в points?
         self.circles = []
+        self.next_circle = None
         self.isPressMarkUpButton = False
         self.isPolyCreated = False
         self.windowId = None
 
     def get_frame_polygon(self, frame):
         polygon_frame = np.copy(frame)
-        points = np.array(self.circles)
+        points = None
+        if self.isPressMarkUpButton and self.next_circle is not None:
+            if len(self.circles) > 0:
+                points = np.append(self.circles, [self.next_circle], axis=0)
+            else:
+                points = np.array([self.next_circle])
+        else:
+            points = np.array(self.circles)
+
         for center_position in self.circles:
             cv2.circle(polygon_frame, center_position, 2, (0, 0, 255), -1)
 
-        cv2.polylines(polygon_frame, np.int32([points]), not self.isPressMarkUpButton, (255, 255, 255), 3)
+        cv2.polylines(polygon_frame, np.int32([points]), True, (255, 255, 255), 3)
         self.isPolyCreated = True
         stencil = np.zeros(polygon_frame.shape).astype(polygon_frame.dtype)
         stencil[:] = (255, 255, 255) # далее белым по белому?
@@ -34,7 +45,7 @@ class BorderDetector:
             polygon_frame = cv2.bitwise_and(polygon_frame, stencil)
         return polygon_frame
 
-    def clearCircles():
+    def clearCircles(self):
             self.circles = []
             self.isPolyCreated = False
 
@@ -42,9 +53,10 @@ class BorderDetector:
         self.isPressMarkUpButton = True
         self.windowId = windowId
         cv2.namedWindow(self.windowId)
-        cv2.setMouseCallback(self.windowId, mouse_drawing, param=self.circles)
+        cv2.setMouseCallback(self.windowId, mouse_drawing, param=self)
 
     def end_selecting_region(self):
+        self.next_circle = None
         self.isPressMarkUpButton = False
         self.isPolyCreated = len(self.circles) >= 3
         if not self.isPolyCreated:
